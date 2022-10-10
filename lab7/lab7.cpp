9e5e17a79731fec33afd5db2157e5cf0193dd9ed
Даниил Алисевич <daniilalisevich@gmail.com>
2022-10-10 17:10:56 +0300

    final lab7

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -9,7 +9,6 @@
 #include <opencv2/video/tracking.hpp>
 #include <opencv2/core/ocl.hpp>
 
-
 #define MINTRACKAREA 50
 
 using namespace std;
@@ -17,7 +16,7 @@ using namespace cv;
 
 void detectAndDisplay(int, void*);
 int displayLukas(const string& filename);
-//int tracking(const string& filename);
+int tracking(const string& filename);
 
 static const int MAX_CORNERS = 1000;
 
@@ -26,7 +25,7 @@ int main(int argc, const char** argv)
 
     detectAndDisplay(0, 0);
     displayLukas("D:/univer/PMMP/image/cars.mp4");
-    //tracking("D:/univer/PMMP/image/cars.mp4");
+    tracking("D:/univer/PMMP/image/cars.mp4");
 }
 
 void detectAndDisplay(int, void*) {
@@ -157,15 +156,73 @@ int displayLukas(const string& filename) {
     }
 }
 
-/*int tracking(const string& filename) {
+int tracking(const string& filename) {
+    // List of tracker types in OpenCV 3.4.1
+    string trackerTypes[8] = { "BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN", "MOSSE", "CSRT" };
+    // vector <string> trackerTypes(types, std::end(types));
+
+    // Create a tracker
+    string trackerType = trackerTypes[1];
+
+    Ptr<Tracker> tracker = TrackerMIL::create();;
+
+    */
+    // Read video
     VideoCapture video(filename);
-    if (!video.isOpened()) {
+
+    // Exit if video is not opened
+    if (!video.isOpened())
+    {
         cout << "Could not read video file" << endl;
         return 1;
-
     }
 
+    // Read first frame 
     Mat frame;
     bool ok = video.read(frame);
 
-}*/
+    // Uncomment the line below to select a different bounding box 
+    Rect bbox = selectROI(frame, false); 
+    // Display bounding box. 
+    rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+
+    imshow("Tracking", frame);
+    tracker->init(frame, bbox);
+
+    while (video.read(frame))
+    {
+        // Start timer
+        double timer = (double)getTickCount();
+
+        // Update the tracking result
+        bool ok = tracker->update(frame, bbox);
+
+        // Calculate Frames per second (FPS)
+        float fps = getTickFrequency() / ((double)getTickCount() - timer);
+
+        if (ok)
+        {
+            // Tracking success : Draw the tracked object
+            rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+        }
+        else
+        {
+            // Tracking failure detected.
+            putText(frame, "Tracking failure detected", Point(100, 80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255), 2);
+        }
+
+        // Display tracker type on frame
+        putText(frame, trackerType + " Tracker", Point(100, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
+
+        // Display frame.
+        imshow("Tracking", frame);
+
+        // Exit if ESC pressed.
+        int k = waitKey(1);
+        if (k == 27)
+        {
+            break;
+        }
+
+    }
+}
