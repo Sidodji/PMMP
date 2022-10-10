2928dfb17bc88022038b99c431bb281f23ca6886
Даниил Алисевич <daniilalisevich@gmail.com>
2022-10-10 16:49:01 +0300

    update lab 7

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,18 +1,32 @@
 ﻿#include <opencv2/video/background_segm.hpp>
+#include <vector>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
-#include <iostream>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/core/ocl.hpp>
+
+
+#define MINTRACKAREA 50
+
 using namespace std;
 using namespace cv;
 
 void detectAndDisplay(int, void*);
+int displayLukas(const string& filename);
+//int tracking(const string& filename);
+
+static const int MAX_CORNERS = 1000;
 
 int main(int argc, const char** argv)
 {
 
     detectAndDisplay(0, 0);
+    displayLukas("D:/univer/PMMP/image/cars.mp4");
+    //tracking("D:/univer/PMMP/image/cars.mp4");
 }
 
 void detectAndDisplay(int, void*) {
@@ -64,3 +78,94 @@ void detectAndDisplay(int, void*) {
         }
     }
 }
+
+int displayLukas(const string& filename) {
+    // Read the video 
+    VideoCapture capture(filename);
+    if (!capture.isOpened()) {
+        //error in opening the video input
+        cerr << "Unable to open file!" << endl;
+        return 0;
+    }
+
+    // Create random colors
+    vector<Scalar> colors;
+    RNG rng;
+    for (int i = 0; i < 100; i++)
+    {
+        int r = rng.uniform(0, 256);
+        int g = rng.uniform(0, 256);
+        int b = rng.uniform(0, 256);
+        colors.push_back(Scalar(r, g, b));
+    }
+
+    Mat old_frame, old_gray;
+    vector<Point2f> p0, p1;
+
+    // Read first frame and find corners in it
+    capture >> old_frame;
+    cvtColor(old_frame, old_gray, COLOR_BGR2GRAY);
+    goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+
+    // Create a mask image for drawing purposes
+    Mat mask = Mat::zeros(old_frame.size(), old_frame.type());
+
+    while (true) {
+        int counter = 0;
+        // Read new frame
+        Mat frame, frame_gray;
+        capture >> frame;
+        if (frame.empty())
+            break;
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+
+        // Calculate optical flow
+        vector<uchar> status;
+        vector<float> err;
+        TermCriteria criteria = TermCriteria((TermCriteria::COUNT)+(TermCriteria::EPS), 10, 0.03);
+        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(15, 15), 2, criteria);
+        vector<Point2f> good_new;
+
+        // Visualization part
+        for (uint i = 0; i < p0.size(); i++)
+        {
+            // Select good points
+            if (status[i] == 1) {
+                good_new.push_back(p1[i]);
+                // Draw the tracks
+                line(mask, p1[i], p0[i], colors[i], 2);
+                circle(frame, p1[i], 5, colors[i], -1);
+            }
+        }
+
+        // Display the demo
+        Mat img;
+        add(frame, mask, img);
+        //if (save) {
+            string save_path = "./optical_flow_frames/frame_" + to_string(counter) + ".jpg";
+            imwrite(save_path, img);
+        //}
+        imshow("flow", img);
+        int keyboard = waitKey(25);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+
+        // Update the previous frame and previous points
+        old_gray = frame_gray.clone();
+        p0 = good_new;
+        counter++;
+    }
+}
+
+/*int tracking(const string& filename) {
+    VideoCapture video(filename);
+    if (!video.isOpened()) {
+        cout << "Could not read video file" << endl;
+        return 1;
+
+    }
+
+    Mat frame;
+    bool ok = video.read(frame);
+
+}*/
